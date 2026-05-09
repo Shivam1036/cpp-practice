ce1ff5cf8d7c1b24ffa9e89d0c37c031d71932aa
Shivam1036 <mshivamkumarmishra544@gmail.com>
2026-05-09 11:29:12 +0530

    merge changes

--- helloc++.cpp
+++ helloc++.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a,b,sum;
+    int a,b,sum=0;
 cout<<"enter  number a\n";
 cin>> a;
 cout<<"enter  number b\n";
 cin>> b;
-sum
+sum=a+b;
 cout<<"sum is "<<sum;
 }
